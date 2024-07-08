3140c0c2d38bc13372651c510ff82b497a6c2bae
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2024-07-08 01:14:16 +0100

    added argument parsing

--- src/duplitrace_indexer/main.cpp
+++ src/duplitrace_indexer/main.cpp
@@ -18,9 +18,46 @@ Copyright 2024 Python Node Editor Development Team
     along with this program.If not, see < https://www.gnu.org/licenses/>.
 */
 #include <cstdlib>
+#include <filesystem>
+#include "argparse.hpp"
 
-// EXIT_SUCCESS and EXIT_FAILURE
+const std::string DEFAULT_CONFIG_FILE = "./config.cfg";
 
 int main(int argc, char **argv) {
+
+    bool verbose = false;
+
+    argparse::ArgumentParser arguments_parser(argv[0]);
+    arguments_parser.add_argument("-c", "--config")
+        .default_value(DEFAULT_CONFIG_FILE)
+        .help("Configuration file");
+    arguments_parser.add_argument ("--verbose")
+        .help ("increase output verbosity")
+        .implicit_value(true)
+        .flag ();
+
+    try {
+        arguments_parser.parse_args (argc, argv);
+    }
+    catch (const std::exception& err) {
+        std::cerr << err.what () << std::endl;
+        std::cerr << arguments_parser;
+        return EXIT_FAILURE;
+    }
+
+    auto config_file = arguments_parser.get<std::string> ("--config");
+    std::cout << "[INFO] Config File : " << config_file << std::endl;
+
+    if (arguments_parser["--verbose"] == true) {
+        std::cout << "[INFO] Setting verbose mode" << std::endl;
+        verbose = true;
+    }
+
+    if (!std::filesystem::is_regular_file(config_file)) {
+        std::cout << "[ERROR] Config file '" << config_file
+            << "' is not a valid file/cannot be read" << std::endl;
+        return EXIT_FAILURE;
+    }
+
     return EXIT_SUCCESS;
 }
